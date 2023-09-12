cd10cbbd145449a1f167e958d0f2cf9d1b906669
MRadhityaU <muhammad.radhitya@ui.ac.id>
2023-09-12 15:10:27 +0700

    added more attributes to model item

--- main/models.py
+++ main/models.py
@@ -5,3 +5,5 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    attack = models.IntegerField(default='0000000', editable=False)
+    defense = models.IntegerField(default='0000000', editable=False)
--- main/views.py
+++ main/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Muhammad Radhitya Utomo',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'description': 'The ultimate wizard in terms of attack and defense',
+        'attack': 2500,
+        'defense': 2100,
+        'amount': 64,
+
     }
 
     return render(request, "main.html", context)
