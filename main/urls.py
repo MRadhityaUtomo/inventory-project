e8e10c37ecee2ee56e5ef45fc96d4f196a40f08e
MRadhityaU <muhammad.radhitya@ui.ac.id>
2023-09-19 21:21:04 +0700

    added by id indexing for xml and json

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_Item, show_xml, show_json 
+from main.views import show_main, add_Item, show_xml, show_json, show_xml_by_id, show_json_by_id 
 
 app_name = 'main'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add-item', add_Item, name='add_item'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
