20abeaf17ae6c2d4bcd8fd378482dec17f340849
MRadhityaU <muhammad.radhitya@ui.ac.id>
2023-09-19 23:45:10 +0700

    fixed the test files

--- main/tests.py
+++ main/tests.py
@@ -3,20 +3,20 @@ from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
-        response = Client().get('/main/')
+        response = Client().get('')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('/main/')
+        response = Client().get('')
         self.assertTemplateUsed(response, 'main.html')
 
     def setUp(self):  #Test pengecekan initiate suatu object model item
         Item.objects.create(name="Journal",amount="1",
-                            flavour_text=
+                            description=
                             "Something to remind, vent to, and cherish",
                             connection="Irreplaceable")
         Item.objects.create(name="Fair ticket",amount="4",
-                            flavour_text=
+                            description=
                             "A fair ticket to the comifuro festival, each one a different party and story",
                             connection="Keepsake")
         
