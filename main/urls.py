b52ee263d87643a010831ff7747c08ebe48591fb
MRadhityaUtomo <radhityautomo26@gmail.com>
2023-09-27 00:23:40 +0700

    Tugas 4 + bonus

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,38 @@
 from django.urls import path
-from main.views import show_main, add_Item, show_xml, show_json, show_xml_by_id, show_json_by_id 
+
+# Data view imports
+from main.views import show_main, add_Item, show_xml, show_json, show_xml_by_id, show_json_by_id
+
+# import register
+from main.views import register
+
+# import login
+from main.views import login_user 
+
+# import logout
+from main.views import logout_user
+
+# import amount editors
+from main.views import add_amount, dec_amount, delete_item
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-item', add_Item, name='add_item'),
+
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+
+    path('tambah/<int:id>/', add_amount, name='add_amount'),
+    path('kurang/<int:id>/', dec_amount, name='dec_amount'),
+    path('hapus/<int:id>/', delete_item, name='delete_item'),
+
+     
 ]
--- main/views.py
+++ main/views.py
@@ -6,25 +6,88 @@ from main.models import Item
 from django.http import HttpResponse
 from django.core import serializers
 
-# Create your views here.
+# Registration form imports
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+
+# Login function imports 
+from django.contrib.auth import authenticate, login
+
+# Logout function imports
+from django.contrib.auth import logout
+
+# Restrict access
+from django.contrib.auth.decorators import login_required
+
+# last login cookie deetector mm
+import datetime
+
+@login_required(login_url='/login')
 def show_main(request):
-    item = Item.objects.all()
+    item = Item.objects.filter(user=request.user)
 
     context = {
-        'name': 'Muhammad Radhitya Utomo',
+        'name': request.user.username,
         'class': 'PBP D',
         'description': 'The ultimate wizard in terms of attack and defense. come and enter your wares traveler, for they will be your memoirs, reminders, keepsakes, of your adventure',
         'items' : item,
         'count' : item.count,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
+
+
+
+# Register function
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+
+# Login function
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+
+
+# logout function
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
 
 def add_Item(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
@@ -45,3 +108,27 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def add_amount(request, id):
+    if request.method == "POST":
+        item = Item.objects.get(pk=id)
+        item.amount += 1
+        item.save()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def dec_amount(request, id):
+    if request.method == "POST":
+        item = Item.objects.get(pk=id)
+        if item.amount > 0:
+            item.amount -= 1
+            item.save()
+        if item.amount == 0:
+            item.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def delete_item(request, id):
+    if request.method == "POST":
+        item = Item.objects.get(pk=id)
+        item.delete()
+    return HttpResponseRedirect(reverse('main:show_main')) 
